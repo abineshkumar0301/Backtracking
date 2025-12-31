d14e8a82c993f8f9a38de6b37dcf2ca764397f06
Abinesh Kumar K <abineshkumar0301@gmail.com>
2025-12-31 11:42:36 +0530

    Printing the subsets of an array using Include/Exclude pattern recursion

---
+++ Subsets.java
@@ -0,0 +1,38 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Subsets {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the length of array");
+
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            System.out.println("Enter the element " + (i + 1));
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.println("Your array : " + Arrays.toString(arr));
+
+        printSubsets(arr, 0, new ArrayList<>());
+
+    }
+
+    private static void printSubsets(int[] arr, int idx, List<Integer> subsets){
+        if(idx == arr.length){
+            System.out.println(subsets);
+            return;
+        }
+        subsets.add(arr[idx]);
+        printSubsets(arr, idx + 1, subsets);
+        subsets.remove(subsets.size() - 1);
+        printSubsets(arr, idx + 1, subsets);
+        
+
+    }
+}
